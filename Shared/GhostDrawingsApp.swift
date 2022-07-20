519799820c48882491421260cff7a802f2afcd4c
GregKeeley <darkscuall@yahoo.com>
2022-07-20 05:18:47 -0400

    Initial Commit

---
+++ Shared/GhostDrawingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhostDrawingsApp.swift
+//  Shared
+//
+//  Created by Gregory Keeley on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct GhostDrawingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
